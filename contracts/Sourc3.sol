ba221831facf55e276272a0185b489d6c1bd2986
roman-strilets <roman@beam-mw.com>
2022-06-24 16:26:25 +0300

    create interface

--- contracts/Sourc3.sol
+++ contracts/Sourc3.sol
@@ -25,6 +25,17 @@ contract Sourc3 {
         bytes32 nameHash_;
     }
 
+    struct GitRef {
+        bytes20 commitHash_;
+        string name_;
+    }
+    
+    struct PackedObject {
+        uint8 type_;
+        bytes20 hash_;
+        bytes data_;
+    }
+    
     mapping (uint64 => Organization) organizations_;
     mapping (uint64 => Project) projects_;
     mapping (uint64 => Repo) repos_;
@@ -36,27 +47,57 @@ contract Sourc3 {
         //msg.sender
     }
 
-    function modifyOrganization() public {
+    function modifyOrganization(uint64 organizationId, string memory name) public {
     }
 
-    function removeOrganzation() public {
+    function removeOrganzation(uint64 organizationId) public {
     }
 
     function createProject(string memory name, uint64 organizationId) public {
     }
 
-    function modifyProject() public {
+    function modifyProject(string memory name, uint64 organizationId, uint64 projectId) public {
     }
 
-    function removeProject() public {
+    function removeProject(uint64 projectId) public {
     }
 
     function createRepo(string memory name, uint64 projectId) public {
     }
 
-    function modifyRepo() public {
+    function modifyRepo(string memory name, uint64 repoId) public {
     }
 
-    function removeRepo() public {
+    function removeRepo(uint64 repoId) public {
     }
+
+    //
+
+    function pushRefs(uint64 repoId, GitRef[] memory refs) public {
+    }
+
+    function pushObjects(uint64 repoId, PackedObject[] memory objects) public {}
+
+    // Repo member
+
+    function addRepoMember(uint64 repoId, address member, uint8 permissions) public {}
+
+    function modifyRepoMember(uint64 repoId, address member, uint8 permissions) public {}
+
+    function removeRepoMeber(uint64 repoId, address member) public {}
+
+    // project member
+
+    function addProjectMember(uint64 projectId, address member, uint8 permissions) public {}
+
+    function modifyProjectMember(uint64 projectId, address member, uint8 permissions) public {}
+
+    function removeProjectMember(uint64 projectId, address member) public {}
+
+    // organization member
+    function addOrganizationMember(uint64 organizationId, address member, uint8 permissions) public {}
+
+    function modifyOrganizationMember(uint64 organizationId, address member, uint8 permissions) public {}
+
+    function removeOrganizationMember(uint64 organizationId, address member) public {}
 }
